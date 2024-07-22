6805a8fb336c7165802c93e1ad08e36e7ce91080
Abhisek Thapa <112496493+abhisekthapam@users.noreply.github.com>
2024-07-22 21:27:15 +0545

    Abhisek/6 (#10)
    * feat : SadTalker
    
    * feat : any face talk
    
    * feat : multiple voices(male,female,child,lyrical,angry)
    
    * chore: update styles and images
    
    ---------
    
    Co-authored-by: abhisekthapam <ahisekmagar250203@gmail.com>

---
+++ any-face-talk/src/components/Livestream.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Livestream() {
+  return (
+    <div className='h-52 text-3xl font-bold flex justify-center items-center'>
+     <a href='http://localhost:3000/agents'>Click me</a>
+    </div>
+  )
+}
+
+export default Livestream
